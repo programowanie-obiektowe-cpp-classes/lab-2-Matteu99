80c68710f2927d855c708b62f5013618d9e35e8b
Matteu99 <mateusz.wawrzyniak.1@gmail.com>
2024-11-04 12:48:36 +0100

    zejsiowka2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,38 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource;
+
+public:
+    
+    ResourceManager()
+        : resource(new Resource()) {}  
+
+    
+    ~ResourceManager()
+    {
+        delete resource;  
+    }
+
+    
+    ResourceManager(const ResourceManager& other)
+        : resource(new Resource(*other.resource)) {}  
+
+    
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other)  
+        {
+            delete resource;  
+            resource = new Resource(*other.resource);  
+        }
+        return *this;
+    }
+
+    
+    double get() const
+    {
+        return resource->get();  
+    }
 };
